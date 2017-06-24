4ebc1cf3c4179c5bb5b071d678b21bd82ac8ab09
Anish Duwal <duwalanise@gmail.com>
2017-06-24 17:51:53 +0545

    redirect to homepage on login through social media

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -78,6 +78,7 @@ class Login extends Component {
   handleSignIn(service) {
     const provider = this.getProvider(service);
     firebase.auth().signInWithPopup(provider)
+      .then(() => { browserHistory.push('/'); })
       .catch((error) => {
         console.log(error);
         this.setState({ hasError: true });
