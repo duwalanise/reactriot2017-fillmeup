b896e7ba09401d3fd9bfbc4751063040a86f9c05
Raman Amatya <ramanamatya@gmail.com>
2017-06-24 09:24:04 +0545

    sample google component in app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,43 @@
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { Link, browserHistory } from 'react-router';
+
+// class App extends Component {
+//   render() {
+//     return <div> hello </div>;
+//   }
+// }
+
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Link, browserHistory } from 'react-router';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class App extends Component {
+  // static defaultProps = {
+  //   center: {lat: 59.95, lng: 30.33},
+  //   zoom: 11
+  // };
+
   render() {
-    return <div> hello </div>;
+    return (
+      <GoogleMapReact
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text={'Kreyser Avrora'}
+        />
+      </GoogleMapReact>
+    );
   }
+}
+
+App.defaultProps = {
+  center: {lat: 59.95, lng: 30.33},
+  zoom: 11,
 }
 
 export default App;
