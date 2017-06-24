6559e161aecc808e67e62250eadd6934d817fafa
rajatamatya <rajatamatya20@gmail.com>
2017-06-24 11:48:00 +0545

    Marker Implementation

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,11 +1,14 @@
 import React, { PropTypes, Component } from 'react';
 import GoogleMap from 'google-map-react';
 import { connect } from 'react-redux';
+import MarkerText from './marker/marker-text.jsx';
 
 class SimpleMapPage extends Component {
   render() {
     return (
-      <GoogleMap center={this.props.center} zoom={this.props.zoom} />
+      <GoogleMap center={this.props.center} zoom={this.props.zoom}>
+        <MarkerText lat={27.6795718} lng={85.3171355} status={'open'} />
+      </GoogleMap>
     );
   }
 }
@@ -21,6 +24,6 @@ SimpleMapPage.defaultProps = {
 };
 
 SimpleMapPage.propTypes = {
-  center: PropTypes.array,
+  center: PropTypes.arrayOf(PropTypes.any),
   zoom: PropTypes.number,
 };
--- src/index.jsx
+++ src/index.jsx
@@ -10,12 +10,12 @@ import '../style/style.scss';
 import store from '../src/store';
 
 const config = {
-  apiKey: "AIzaSyBIaSiMh-NgPcs2CW5jzRgmPJE_Y69XEpE",
-  authDomain: "fill-me-up-f155d.firebaseapp.com",
-  databaseURL: "https://fill-me-up-f155d.firebaseio.com",
-  projectId: "fill-me-up-f155d",
-  storageBucket: "fill-me-up-f155d.appspot.com",
-  messagingSenderId: "752077583780",
+  apiKey: 'AIzaSyBIaSiMh-NgPcs2CW5jzRgmPJE_Y69XEpE',
+  authDomain: 'fill-me-up-f155d.firebaseapp.com',
+  databaseURL: 'https://fill-me-up-f155d.firebaseio.com',
+  projectId: 'fill-me-up-f155d',
+  storageBucket: 'fill-me-up-f155d.appspot.com',
+  messagingSenderId: '752077583780',
 };
 
 firebase.initializeApp(config);
