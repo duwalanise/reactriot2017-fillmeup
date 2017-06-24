05e0d7b1ba6c37efd8acc9f3c807e5a1c69518b0
Anish Duwal <duwalanise@gmail.com>
2017-06-24 09:45:05 +0545

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,13 @@ import * as firebase from 'firebase';
 import App from './components/app.jsx';
 import SimpleMap from './components/map.jsx';
 
-var config = {
+const config = {
   apiKey: "AIzaSyBIaSiMh-NgPcs2CW5jzRgmPJE_Y69XEpE",
   authDomain: "fill-me-up-f155d.firebaseapp.com",
   databaseURL: "https://fill-me-up-f155d.firebaseio.com",
   projectId: "fill-me-up-f155d",
   storageBucket: "fill-me-up-f155d.appspot.com",
-  messagingSenderId: "752077583780"
+  messagingSenderId: "752077583780",
 };
 
 firebase.initializeApp(config);
