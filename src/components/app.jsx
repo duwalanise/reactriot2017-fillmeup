8b96a10080b66a586ae6ab4dab9869253c0b903f
Dhanit Paija <dhanit.paija@gmail.com>
2017-06-24 09:04:27 +0545

    Init redux

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store from '../store';
 
 class App extends Component {
   render() {
-    return <div> hello </div>;
+    <Provider store={store}>
+      <div>Hello</div>
+    </Provider>
   }
 }
 
