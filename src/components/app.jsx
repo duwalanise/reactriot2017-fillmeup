8b73492ae494ad25052c39efcba01d5cc932b500
Anish Duwal <duwalanise@gmail.com>
2017-06-24 08:44:38 +0545

    initialize firebase in react

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link, browserHistory } from 'react-router';
+
+class App extends Component {
+  render() {
+    return <div> hello </div>;
+  }
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import * as firebase from 'firebase';
+import App from './components/app.jsx';
+
+var config = {
+  apiKey: "AIzaSyBIaSiMh-NgPcs2CW5jzRgmPJE_Y69XEpE",
+  authDomain: "fill-me-up-f155d.firebaseapp.com",
+  databaseURL: "https://fill-me-up-f155d.firebaseio.com",
+  projectId: "fill-me-up-f155d",
+  storageBucket: "fill-me-up-f155d.appspot.com",
+  messagingSenderId: "752077583780"
+};
+
+firebase.initializeApp(config);
+
+render(<App />, document.getElementById('app'));
