e0d5fd77fffbd4478388a5548d857eb52ef30b54
Dhanit Paija <dhanit.paija@gmail.com>
2017-06-26 00:03:31 +0545

    Add search bar specs

--- test/pump_detail_spec.js
+++ test/pump_detail_spec.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
-import proxyquire from 'proxyquire';
 import { PumpDetail } from '../src/components/pump_detail.jsx';
 
 describe('<PumpDetail />', () => {
