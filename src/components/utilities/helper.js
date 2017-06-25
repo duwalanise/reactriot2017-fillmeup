2c89d821b3e3d08b014b5e3b80a7e0152254399b
rajatamatya <rajatamatya20@gmail.com>
2017-06-25 10:11:48 +0545

    Handle Marker Info Close on Switch

--- src/components/utilities/helper.js
+++ src/components/utilities/helper.js
@@ -2,3 +2,11 @@ export const setDashOnNull = (value) => {
   const result = (value) || '-';
   return result;
 };
+
+export const closeMarkerInfo = (pumpDetails) => {
+  const data = pumpDetails.map((pumpInfo) => {
+    pumpInfo.isMarkerOpen = false;
+    return pumpInfo;
+  });
+  return data;
+};
